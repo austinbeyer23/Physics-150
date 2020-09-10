b2ed716cbe5d8b7ad79328c3f16763cecc7b1e42
austinbeyer23 <70345000+austinbeyer23@users.noreply.github.com>
2020-09-10 10:25:56 -0500

    Update RedWhiteBlueSnoring.py

--- RedWhiteBlueSnoring.py
+++ RedWhiteBlueSnoring.py
@@ -11,34 +11,34 @@ from adafruit_circuitplayground import cp
  
 
 while True:
-    length = 255 #sets the length of the snore
+    length = 15 #sets the length of the snore
     j=0
     wait_down = .01
     wait_up = .01
     while (j<length):
-        cp.pixels[0]=(255-j, 0, 0)
-        cp.pixels[9]=(255-j, 0, 0)
-        cp.pixels[1]=(0, 0,255-j)
-        cp.pixels[8]=(0, 0,255-j)
-        cp.pixels[2]=(255-j,255-j,255-j)
-        cp.pixels[7]=(255-j,255-j,255-j)
-        cp.pixels[3]=(0, 0,255-j)
-        cp.pixels[6]=(0, 0,255-j)
-        cp.pixels[4]=(255-j, 0, 0)
-        cp.pixels[5]=(255-j, 0, 0)
+        cp.pixels[0]=(150-j, 0, 0)
+        cp.pixels[9]=(150-j, 0, 0)
+        cp.pixels[1]=(0, 0,15-j)
+        cp.pixels[8]=(0, 0,15-j)
+        cp.pixels[2]=(15-j,15-j,15-j)
+        cp.pixels[7]=(15-j,15-j,15-j)
+        cp.pixels[3]=(0, 0,15-j)
+        cp.pixels[6]=(0, 0,15-j)
+        cp.pixels[4]=(150-j, 0, 0)
+        cp.pixels[5]=(150-j, 0, 0)
         j = j+1
         time.sleep(wait_down)
     time.sleep(.01)
     while (j>0):
-        cp.pixels[0]=(255-j, 0, 0)
-        cp.pixels[9]=(255-j, 0, 0)
+        cp.pixels[0]=(50-j, 0, 0)
+        cp.pixels[9]=(50-j, 0, 0)
         cp.pixels[1]=(0, 0,255-j)
         cp.pixels[8]=(0, 0,255-j)
-        cp.pixels[2]=(255-j,255-j,255-j)
-        cp.pixels[7]=(255-j,255-j,255-j)
+        cp.pixels[2]=(15-j,15-j,15-j)
+        cp.pixels[7]=(15-j,15-j,15-j)
         cp.pixels[3]=(0, 0,255-j)
         cp.pixels[6]=(0, 0,255-j)
-        cp.pixels[4]=(255-j, 0, 0)
-        cp.pixels[5]=(255-j, 0, 0)
+        cp.pixels[4]=(50-j, 0, 0)
+        cp.pixels[5]=(50-j, 0, 0)
         j = j-1
         time.sleep(wait_up)
